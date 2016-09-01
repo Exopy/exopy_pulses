990acd95e135d1a43c67d1c30ec1532dbbda29ed
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-09-01 15:57:44 +0200

    pulses : add a normalizer for shape names

--- ecpy_pulses/pulses/utils/normalizers.py
+++ ecpy_pulses/pulses/utils/normalizers.py
@@ -6,18 +6,22 @@
 #
 # The full license is in the file LICENCE, distributed with this software.
 # -----------------------------------------------------------------------------
-"""
+"""Utility functions to build user-friendly names from ids.
+
+Those should be use as formatting function argument to the ids_to_unique_names
+function found in ecpy.utils.transformers
 
 """
 from __future__ import (division, unicode_literals, print_function,
                         absolute_import)
 
-# XXXX should use the transformers from ecpy
+
 # TODO would be nicer with regex but I never managed to get the regex right.
 def normalize_sequence_name(name):
-    """Normalize names.
+    """Normalize sequence names.
 
-    For tasks, replaces '_' by spaces and add spaces between 'aA' sequences.
+    For sequences, strip the terminal Sequence if present, replaces '_' by
+    spaces and add spaces between 'aA' sequences.
     For templates, only the extension file is removed.
 
     """
@@ -62,13 +66,54 @@ def normalize_sequence_name(name):
 
 # TODO would be nicer with regex but I never managed to get the regex right.
 def normalize_context_name(name):
-    """Normalize names.
+    """Normalize context names.
 
-    For tasks, replaces '_' by spaces and add spaces between 'aA' sequences.
-    For templates, only the extension file is removed.
+    Strip terminal Context and replaces '_' by spaces and add spaces between
+    'aA' sequences.
 
     """
     if name.endswith('Context'):
+        name = name[:-7] + '\0'
+
+    package = None
+    if '.' in name:
+        package, name = name.rsplit('.', 1)
+
+    aux = ''
+    for i, char in enumerate(name):
+        if char == '_':
+            aux += ' '
+            continue
+
+        if char != '\0':
+            if char.isupper() and i != 0:
+                if name[i-1].islower():
+                    if name[i+1].islower():
+                        aux += ' ' + char.lower()
+                    else:
+                        aux += ' ' + char
+                elif i + 1 < len(name):
+                    if name[i+1].islower():
+                        aux += ' ' + char.lower()
+                    else:
+                        aux += char
+            else:
+                if i == 0:
+                    aux += char.upper()
+                else:
+                    aux += char
+
+    return package + '.' + aux if package else aux
+
+
+def normalize_shape_name(name):
+    """Normalize shape names.
+
+    Strip terminal Shape and replaces '_' by spaces and add spaces between
+    'aA' sequences.
+
+    """
+    if name.endswith('Shape'):
         name = name[:-7] + '\0'
 
     package = None
