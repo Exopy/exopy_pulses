11c59e285b374872d2da2912e2b87cd19ae9d855
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-09-07 17:24:58 +0200

    pulses : fix broken tests

--- ecpy_pulses/testing/context.py
+++ ecpy_pulses/testing/context.py
@@ -26,12 +26,11 @@ class TestContext(BaseContext):
 
     sampling = Float(1.0)
 
-    def compile_and_transfer_sequence(self, item, driver=None):
+    def compile_and_transfer_sequence(self, items, driver=None):
         """Simply evaluate and simplify the underlying sequence.
 
         """
-        seq = item.evaluate_sequence()
-        return seq.simplify_sequence()
+        return True, {}
 
     def _get_sampling_time(self):
         return self.sampling
